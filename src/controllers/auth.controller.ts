ab90e11ad17ce2797b3ed4ae8e56ce7686e9c8d3
alandas71 <alanalves.dev@gmail.com>
2025-10-17 09:04:21 -0300

    Modelagem nova

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -37,7 +37,8 @@ export class AuthController {
       res.cookie('auth_token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
-        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+        // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        sameSite: 'none',
         domain: process.env.NODE_ENV === 'production' ? '.planvita.com.br' : undefined,
         maxAge: 1000 * 60 * 60 * 24, // 1 dia
       });
